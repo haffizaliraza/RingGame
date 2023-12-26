de868ae982d1f01bcbd7a6f7ab1003adaef73320
aliraza <haffizaliraza@gmail.com>
2023-12-26 19:05:37 +0500

    add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
   devise_for :users
-  authenticated :user do
-    root to: 'dashboard#index', as: :authenticated_root
-  end
-
-  root to: 'welcome#index'
+  root to: 'dashboard#index', as: :authenticated_root
 end
