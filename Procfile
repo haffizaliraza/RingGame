04f2ce7cfcff3b669599282bbb3c3c777efa9696
ali raza <haffizaliraza@gmail.com>
2024-02-29 00:33:25 +0500

    update procfile with gitignore

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec puma -C config/puma.rb
+web: bundle exec rake db:migrate db:seed && bundle exec puma -C config/puma.rb
--- db/seeds.rb
+++ db/seeds.rb
@@ -92,3 +92,26 @@ User.where.not(team_id: nil).each_with_index do |user, index|
     game.save!
   end
 end
+
+Team.all.each do |team|
+  next if team.games.length < 1
+  team_rank = TeamRank.find_or_initialize_by(team_id: team.id)
+  success_rate = team.current_success_rate
+  team_rank.success_rate = success_rate
+  team_rank.save
+end
+
+User.where(team_id: nil).each do |user|
+  next if user.games.length < 1
+  if user.gender == 'male'
+    user_rank = MaleRank.find_or_initialize_by(user_id: user.id)
+    success_rate = user.current_success_rate
+    user_rank.success_rate = success_rate
+    user_rank.save
+  else
+    user_rank = FemaleRank.find_or_initialize_by(user_id: user.id)
+    success_rate = user.current_success_rate
+    user_rank.success_rate = success_rate
+    user_rank.save
+  end
+end
